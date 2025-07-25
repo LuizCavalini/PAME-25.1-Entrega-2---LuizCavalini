cc0c92e4c24fc36e5a3c97691fbdac924ea898b9
LuizCavalini <felipecavalini@outlook.com>
2025-07-25 20:00:10 -0300

    função cadastro cliente

--- Cliente.js
+++ Cliente.js
@@ -1,8 +1,8 @@
 class Cliente{
     constructor(id, nome, dataNascimento, cpf, email, senha){
-         this.id = id;
-    this.nome = nome;
-    this.dataNascimento = dataNascimento;
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
     }
 }
 module.exports = Cliente;
