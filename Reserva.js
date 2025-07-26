589ec14254e065b48f670c9180ac3d0e16866957
LuizCavalini <felipecavalini@outlook.com>
2025-07-25 21:19:40 -0300

    função Cancelar reserva

--- Reserva.js
+++ Reserva.js
@@ -1,7 +1,8 @@
 class Reserva{
     constructor(id, idCliente, status, dataEntrada, dataSaida){
         this.id = id; 
-        this.idCliente = idCliente; 
+        this.idCliente = idCliente;
+        this.nomeQuarto = this.nomeQuarto; 
         this.status = status; 
         this.dataEntrada = dataEntrada; 
         this.dataSaida = dataSaida; 
